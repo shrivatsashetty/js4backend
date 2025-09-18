b4fe7c8dae0a7c08ed082433dba5332b2d52be46
shrivatsashetty <shrivatsa307@gmail.com>
2025-09-18 17:15:04 +0530

    demo projects for Common JS Modules and ES6 Modules

--- Common-JS-Modules/main.js
+++ Common-JS-Modules/main.js
@@ -4,6 +4,9 @@ const {C, PI} = require("./utils");
 console.log("PI:", PI);
 console.log("c:", C);
 
+/* some variabels like __dirname, __filename the the object module
+along with the function export and require are auto imported by Node.js 
+and hence we can use them directly */
 console.log("Current Directory:\n", __dirname);
 console.log("Current File:\n", __filename);
 
