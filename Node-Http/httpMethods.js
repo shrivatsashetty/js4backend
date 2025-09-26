b431e818d25cc019b4f13b174ac8e9841c3ed435
shrivatsashetty <shrivatsa307@gmail.com>
2025-09-26 23:27:06 +0530

    creating a simple API in Node.js

--- Node-Http/httpMethods.js
+++ Node-Http/httpMethods.js
@@ -1,8 +1,10 @@
 /* run the below server and then visit the URL:
  * http://localhost:49519/
- * http://localhost:49519/todos */
+ * http://localhost:49519/todos 
+ * */
 
 import http from "http";
+import path from "path";
 
 import {URL} from "url";
 
@@ -11,7 +13,7 @@ const PORT = 49519;
 // In-memory data store (for demonstration)
 let todos = [
     { id: 1, task: 'Learn Node.js', completed: false },
-    { id: 2, task: 'Build an API', completed: false }
+    { id: 2, task: 'Build an HTTP Server', completed: false }
 ];
 
 const server = http.createServer((req, res) => {
@@ -39,15 +41,76 @@ const server = http.createServer((req, res) => {
 
     // handle GET /todos
     if (method === "GET") {
+
+        res.writeHead(200, {"content-type": "application/json"});
+
         if (pathname === "/todos") {
-            res.writeHead(200, {"content-type": "application/json"});
             res.end(JSON.stringify(todos));
         }
-        // else if (pathname === "") {
-        //         res.writeHead(200, {"content-type": "text/html"});
-        //         res.end("<h1>Namaste Bharath</h1>");
-        // }
+        else if (pathname === "/") {
+            /* setting a response for default GET request */
+            res.end(JSON.stringify({pathname}));
+        }
     }
+    // handling POST request
+    else if (method === "POST" && pathname === '/todos') {
+        let body = "";
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        });
+
+        req.on(
+            "end",
+            () => {
+                try {
+                    let newTodo = JSON.parse(body);
+                    newTodo.id = todos.length + 1;
+                    todos.push(newTodo);
+                    res.writeHead(201, {"content-type": "application/json"});
+                    res.end(JSON.stringify(newTodo));
+                } 
+                catch (err) {
+                    res.writeHead(400, {"content-type": "application/json"});
+                    res.end(JSON.stringify({error: "Invalid JSON"}));
+                }
+            }
+        );
+    }
+    // route: PUT /todos/:id
+    else if (method === "PUT" && pathname.startsWith("/todos/")) {
+        const id = pathname.split("/")[2];
+
+        let body = "";
+
+        req.on("data", (dataChunk) => {
+            body += dataChunk.toString();
+        });
+
+
+        req.on("end", () => {
+            try {
+
+                const updatedToDo = JSON.parse(body);
+                const index = todos.findIndex(todo => todo.id == id);
+    
+                if (index === -1) {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ error: 'Todo not found' }));
+                }
+                else {
+                    todos[index] = {...todos[index], ...updatedToDo} // merging objects using spread operator
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(todos[index]));
+                }
+            } 
+            catch (error) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Invalid JSON' }));
+            }
+
+        });
+    }
+
 
 
 });
@@ -55,3 +118,21 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+
+/* curl commands:
+
+for GET:
+curl http://localhost:49519/todos
+
+for POST:
+curl -X POST http://localhost:49519/todos \
+-H "Content-Type: application/json" \
+-d '{"task":"Learn Express.Js","completed":false}'
+
+for PUT:
+curl -X PUT http://localhost:49519/todos/2 \
+-H "Content-Type: application/json" \
+-d '{ "task": "Build a simple API", "completed": true}'
+
+*/
